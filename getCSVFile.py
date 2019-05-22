34f923ca937f661041afa714dc713943f6a8d728
Siddharth2016 <siddharthchandragzb@gmail.com>
2019-05-22 16:31:23 +0530

    Variable name changed

--- getCSVFile.py
+++ getCSVFile.py
@@ -2,9 +2,9 @@ from urllib.request import urlopen
 from io import StringIO
 import csv
 
-csvdata = urlopen("http://insight.dev.schoolwires.com/HelpAssets/C2Assets/C2Files/C2ImportCalEventSample.csv").read().decode("ascii", "ignore")
-csvdatafile = StringIO(csvdata)
-csvreader = csv.reader(csvdatafile)
+csvData = urlopen("http://insight.dev.schoolwires.com/HelpAssets/C2Assets/C2Files/C2ImportCalEventSample.csv").read().decode("ascii", "ignore")
+csvDatafile = StringIO(csvdata)
+csvReader = csv.reader(csvdatafile)
 
-for row in csvreader:
+for row in csvReader:
     print(row)
